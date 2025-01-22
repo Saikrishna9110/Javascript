0c21255e577bc695773ebf9a70f7fca0eca87fc0
Saikrishna9110 <90841277+Saikrishna9110@users.noreply.github.com>
2025-01-22 14:38:19 +0000

    Datatypes

---
+++ 01_basics/Datatypes.js
@@ -0,0 +1,25 @@
+let name="Saikrishna"
+console.log(typeof name);  // gives string.
+
+let x=45
+console.log(typeof x); //gives number.
+
+//we have many more datatypes
+
+//number
+//bigint
+//string
+//boolean
+//null ==> standalone value
+//undefined ==> variable id declared but not been assigned a value
+
+
+//object
+
+
+console.log(typeof null); //gives as object.
+
+console.log(typeof undefined); // gives as undefined.
+
+//remembering the types of null and undefined will be helpfull in the interviews.
+
